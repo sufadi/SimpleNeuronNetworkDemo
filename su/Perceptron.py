625923fc5d32dac07b0d02c2a387657f66a53cf7
sufadi <332157261@qq.com>
2018-01-24 23:53:27 +0800

    data visible by plt

--- su/Perceptron.py
+++ su/Perceptron.py
@@ -86,7 +86,7 @@ class Perceptron(object):
         #    np.where(1)
         # else:
         #    np.where(-1)
-        return np.where(self.net_input(x) >= 0.0, 1, 1)
+        return np.where(self.net_input(x) >= 0.0, 1, -1)
         pass
     pass
 
@@ -96,12 +96,12 @@ file = "D:/EclipseProject/PythonStudyBySu/su/iris.data.csv"
 # 无文件头
 df = pd.read_csv(file, header=None)
 # 读取前面 10 行数据
-print(df.head(10))
+# print(df.head(10))
 
 # 数据可视化展示
 import matplotlib.pyplot as plt
 
-y = df.loc[0:100, 4].values
+y = df.loc[0:99, 4].values
 #print("显示第四列前100条数据", y)
 y = np.where(y == "Iris-setosa", -1, 1)
 #print("对数据进行分类", y)
@@ -113,51 +113,70 @@ x = df.iloc[0:100, [0, 2]].values
 # 画出图形
 # x 的第一列为x轴，第二列为y轴
 # 前50条数据
-plt.scatter(x[0:50, 0], x[0:50, 1], color='red', marker='o', label='setosa')
+#plt.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
 # 后50条数据
-plt.scatter(x[50:100, 0], x[50:100, 1], color='blue',
-            marker='x', label='versicolor')
-plt.xlabel("花瓣长度")
-plt.ylabel("花茎长度")
-plt.legend(loc='upper left')
+# plt.scatter(x[50:100, 0], x[50:100, 1], color='blue',
+#            marker='x', label='versicolor')
+# plt.xlabel("花瓣长度")
+# plt.ylabel("花茎长度")
+#plt.legend(loc='upper left')
 # plt.show()
 
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(x, y)
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel("error classify count")
+#plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+# plt.xlabel('Epochs')
+#plt.ylabel("error classify count")
 # plt.show()
-
 
 from matplotlib.colors import ListedColormap
 
 
-def plot_decision_regions(x, y, classifier, resolution=0.02):
-    marker = ('s', 'x', 'o', 'v')
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('s', 'x', 'o', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     # y 的种类只有-1 和 1 ，根据相应的种类绘制对应的颜色
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
-    x1_min = x[:, 0].min() - 1
-    x1_max = x[:, 0].max()
-    x2_min = x[:, 1].min() - 1
-    x2_max = x[:, 1].max()
+    x1_min = X[:, 0].min() - 1
+    x1_max = X[:, 0].max()
+    x2_min = X[:, 1].min() - 1
+    x2_max = X[:, 1].max()
 
-    print("花瓣长度最小值 为 %s， 最大值为 %s" % (x1_min, x1_max))
-    print("花茎长度最小值 为 %s， 最大值为 %s" % (x2_min, x2_max))
+    #print("花瓣长度最小值 为 %s， 最大值为 %s" % (x1_min, x1_max))
+    #print("花茎长度最小值 为 %s， 最大值为 %s" % (x2_min, x2_max))
 
     # 构造向量，扩展成一个二维矩阵，resolution向量差值
     xx1, xx2 = np.meshgrid(
         np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-    print("x1向量大小", np.arange(x1_min, x1_max, resolution).shape)
-    print("x1向量", np.arange(x1_min, x1_max, resolution))
-    print("x2向量大小", np.arange(x2_min, x2_max, resolution).shape)
-    print("x2向量", np.arange(x2_min, x2_max, resolution))
+    #print("x1向量大小", np.arange(x1_min, x1_max, resolution).shape)
+    #print("x1向量", np.arange(x1_min, x1_max, resolution))
+    #print("x2向量大小", np.arange(x2_min, x2_max, resolution).shape)
+    #print("x2向量", np.arange(x2_min, x2_max, resolution))
 
-    print("xx1 二维矩阵大小", xx1.shape)
-    print("xx1 二维矩阵", xx1)
-    print("xx2 二维矩阵大小", xx2.shape)
-    print("xx2 二维矩阵", xx2)
+    #print("xx1 二维矩阵大小", xx1.shape)
+    #print("xx1 二维矩阵", xx1)
+    #print("xx2 二维矩阵大小", xx2.shape)
+    #print("xx2 二维矩阵", xx2)
+
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    print("xx1还原成单位向量:", xx1.ravel())
+    print("xx2还原成单位向量:", xx2.ravel())
+    print('分类后的模式数据', z)
+
+    # 转换为二维矩阵
+    z = z.reshape(xx1.shape)
+    # 数据画分类线
+    plt.contourf(xx1, xx2, z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, c1 in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == c1, 0], y=X[y == c1, 1], alpha=0.8, c=cmap(
+            idx), marker=markers[idx], label=c1)
 
 plot_decision_regions(x, y, ppn, resolution=0.02)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(loc='upper left')
+plt.show()
